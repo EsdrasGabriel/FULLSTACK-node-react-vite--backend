4bf11c580dd4586ba0ccdec42eb7852eeda80f09
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-10-27 13:15:41 -0300

    env variables and build config

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -4,11 +4,11 @@ import { StatusCodes } from "http-status-codes";
 const router = Router();
 
 router.get("/", (req, res) => {
-  return res.send("Olá Dev!");
+    return res.send("Olá Dev!");
 });
 
 router.post("/teste", (req, res) => {
-  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+    return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
 });
 
 
