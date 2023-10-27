a83535f12e2530e6f75a2cbf4be95ef02a58932f
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-10-27 14:20:30 -0300

    created create controller and entry types

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { cidadesController, } from "./../controllers";
+
 const router = Router();
 
 router.get("/", (req, res) => {
-    return res.send("Olá Dev!");
+  return res.send("Olá Dev!");
 });
 
-router.post("/teste", (req, res) => {
-    return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
-});
+router.post(
+  "/cidades",
+  cidadesController.create   
+);
 
 export { router };
