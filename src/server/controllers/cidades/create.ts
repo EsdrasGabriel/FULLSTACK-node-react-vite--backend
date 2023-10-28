d4e12326324789bd42b57ca76d255eea99c48aef
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-10-28 12:25:31 -0300

    created simple validation with yup

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,13 +1,35 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface ICidade {
   nome: string;
+  estado: string;
 }
 
-export const create = (req: Request, res: Response) => {
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
+});
 
-  const data: ICidade = req.body;
-  console.log(data.nome);
+export const create = async (req: Request, res: Response) => {
+  let validatedData: ICidade | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message
+      }
+    });
+  }
+
+  console.log(validatedData);
+
+  
 
   return res.send("Create!");
 };
