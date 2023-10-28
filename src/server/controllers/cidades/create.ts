ca7108a01e7cdb30d965cbb67e7bd45c2737bfae
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-10-28 12:55:08 -0300

    created all errors message and translations

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -3,33 +3,35 @@ import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 
 interface ICidade {
-  nome: string;
-  estado: string;
+    nome: string;
+    estado: string;
 }
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
-  nome: yup.string().required().min(3),
-  estado: yup.string().required().min(3),
+    nome: yup.string().required().min(3),
+    estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request, res: Response) => {
-  let validatedData: ICidade | undefined = undefined;
+    let validatedData: ICidade | undefined = undefined;
 
-  try {
-    validatedData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
+    try {
+        validatedData = await bodyValidation.validate(req.body, {
+            abortEarly: false,
+        });
+    } catch (err) {
+        const yupError = err as yup.ValidationError;
+        const errors: Record<string, string> = {};
 
-    return res.json({
-      errors: {
-        default: yupError.message
-      }
-    });
-  }
+        yupError.inner.forEach((error) => {
+            if (!error.path) return;
+            errors[error.path] = error.message;
+        });
 
-  console.log(validatedData);
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+    }
 
-  
+    console.log(validatedData);
 
-  return res.send("Create!");
+    return res.send("Create!");
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,17 +1,14 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 
-import { cidadesController, } from "./../controllers";
+import { cidadesController } from "./../controllers";
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  return res.send("Olá Dev!");
+    return res.send("Olá Dev!");
 });
 
-router.post(
-  "/cidades",
-  cidadesController.create   
-);
+router.post("/cidades", cidadesController.create);
 
 export { router };
