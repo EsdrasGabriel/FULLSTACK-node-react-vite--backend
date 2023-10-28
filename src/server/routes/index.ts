e2989456310c14ab710e497c5b7c84e5ef8afc7a
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-10-28 19:10:20 -0300

    feat: added getById, deleteById and updateById to cidades controller

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,15 +8,30 @@ router.get("/", (req, res) => {
     return res.send("Olá Dev!");
 });
 
+router.post(
+    "/cidades",
+    cidadesController.createValidation,
+    cidadesController.create
+);
 router.get(
     "/cidades",
     cidadesController.getAllValidation,
     cidadesController.getAll
 );
-router.post(
-    "/cidades",
-    cidadesController.createValidation,
-    cidadesController.create
+router.get(
+    "/cidades/:id",
+    cidadesController.getByIdValidation,
+    cidadesController.getById
+);
+router.put(
+    "/cidades/:id",
+    cidadesController.updateByIdValidation,
+    cidadesController.updateById
+);
+router.delete(
+    "/cidades/:id",
+    cidadesController.deleteByIdValidation,
+    cidadesController.deleteById
 );
 
 export { router };
