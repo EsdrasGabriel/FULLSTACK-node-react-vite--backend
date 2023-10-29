0eb2316f4f3f98667dd80aa25fa261507df7eeb3
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-10-29 18:05:49 -0300

    fix: improved jest setup to auto migrate knex database

--- tests/src/server/controllers/cidades/create.test.ts
+++ tests/src/server/controllers/cidades/create.test.ts
@@ -1,5 +1,5 @@
 import { StatusCodes } from "http-status-codes";
-import { testServer } from "../jest.setup";
+import { testServer } from "../../../../jest.setup";
 
 describe("Cidades - Create", () => {
     it ("Criar registro", async () => {
--- tests/src/server/controllers/cidades/deleteById.test.ts
+++ tests/src/server/controllers/cidades/deleteById.test.ts
@@ -1,5 +1,5 @@
 import { StatusCodes } from "http-status-codes";
-import { testServer } from "../jest.setup";
+import { testServer } from "../../../../jest.setup";
 
 describe("Cidades - Delete By Id", () => {
     it ("Deletar Registro Existente", async () => {
