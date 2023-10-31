b9899d3c90e9a4853c181b3d4bdf78284417695f
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-10-31 15:17:33 -0300

    feat: added environment config for production

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -25,5 +25,19 @@ export const test: Knex.Config = {
     connection: ":memory:",
 };
 export const production: Knex.Config = {
-    ...development,
+    client: "pg",
+    migrations: {
+        directory: path.resolve(__dirname, "..", "migrations")
+    },
+    seeds: {
+        directory: path.resolve(__dirname, "..", "seeds")
+    },
+    connection: {
+        host: process.env.DATABASE_HOST,
+        database: process.env.DATABASE_NAME,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        port: Number(process.env.DATABASE_PORT || 5432),
+        ssl: {rejectUnauthorized: false},
+    },
 };
